72b62ed9cad00df232f82d8b726ae927a356b70d
Paolo Tagliaferri <paolo1.tagliaferri@gmail.com>
2026-01-06 00:17:25 +0100

    Fix relative URLs adding `/blog`

--- src/site.config.ts
+++ src/site.config.ts
@@ -34,15 +34,15 @@ export const siteConfig: SiteConfig = {
 // Used to generate links in both the Header & Footer.
 export const menuLinks: { path: string; title: string }[] = [
 	{
-		path: "/",
+		path: "/blog",
 		title: "Home",
 	},
 	{
-		path: "/about/",
+		path: "/blog/about/",
 		title: "About",
 	},
 	{
-		path: "/posts/",
+		path: "/blog/posts/",
 		title: "Blog",
 	},
 ];
